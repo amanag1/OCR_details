9f09b608584b087bc80295ed81bf1477da522b16
amanag1 <aaaamanagarwala1996@gmail.com>
2021-09-04 22:53:44 +0530

    Add files via upload
    New files added to repo

---
+++ ocr_code.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+import os
+from flask.globals import request
+import pandas as pd
+import bs_lgc
+
+app = Flask(__name__)
+
+
+@app.route('/search/text',methods=['POST'])
+def search_text():
+    request_data = request.get_json()
+    coordinate_list = eval(request_data['body']['position'])
+    x0,y0,x2,y2 = coordinate_list[0],coordinate_list[1],coordinate_list[2],coordinate_list[3]
+    fname = request_data['body']['file_name']
+    result = ' '.join(bs_lgc.get_text(x0,y0,x2,y2,fname))
+    response = {'text':result}
+    return response
+
+if __name__ == "__main_":
+    app.run(host='localhost',debug=True,port=5000)
